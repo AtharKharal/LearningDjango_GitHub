30694c1b91ed0ad9f49f096e862d84cc780c2158
Athar Kharal <atharkharal@gmail.com>
2018-05-27 09:09:55 +0500

    Django app building

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -3,6 +3,7 @@ Definition of urls for BasicProject.
 """
 
 from django.conf.urls import include, url
+import HelloDjangoApp.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -18,4 +19,10 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+]
+
+# Django processes URL patterns in the order they appear in the array
+urlpatterns = [
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
 ]
